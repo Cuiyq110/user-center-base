844df03b851e71a9eced69053beba0cea7a6bd85
cuiyq1 <cyq1102051001@gmail.com>
2024-11-23 15:40:45 +0800

    Initial commit

---
+++ src/main/java/com/cuiyq/usercenterbase/UserCenterBaseApplication.java
@@ -0,0 +1,13 @@
+package com.cuiyq.usercenterbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCenterBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterBaseApplication.class, args);
+    }
+
+}
