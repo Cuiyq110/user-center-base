adf0ff2f7840da84ef437d246a8c9469cc005e73
cuiyq1 <cyq1102051001@gmail.com>
2024-11-23 15:42:03 +0800

    add:初始化

--- src/main/java/com/cuiyq/usercenterbase/UserCenterBaseApplication.java
+++ src/main/java/com/cuiyq/usercenterbase/UserCenterBaseApplication.java
@@ -1,9 +1,11 @@
 package com.cuiyq.usercenterbase;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cuiyq.usercenterbase.mapper")
 public class UserCenterBaseApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/cuiyq/usercenterbase/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.cuiyq.usercenterbase.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cuiyq.usercenterbase.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
